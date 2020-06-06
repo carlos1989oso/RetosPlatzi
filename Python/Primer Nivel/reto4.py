4a02d02ac793beaa0564f58530463d0828aad0ff
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 02:41:46 -0500

    reto 4

--- Python/Primer Nivel/reto4.py
+++ Python/Primer Nivel/reto4.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+def sumatoriaNum(num1, num2):
+  return (num1 + num2)
+
+print("Bienvenido a la calculadora\n")
+num1 = float(input('Escribe el primer numero: '))
+num2 = float(input('Escribe el segundo numero: '))
+total = round(sumatoriaNum(num1,num2),2)
+print("\n")
+print("El resultado de sumar " + str(round(num1,2)) + " mas " + str(round(num2,2)) + " es: " + str(total))
