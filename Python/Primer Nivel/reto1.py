b158a698ecf056ade39659c8a9a76e9f4a6497b2
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 02:21:35 -0500

    reto 1 y 2 solve

---
+++ Python/Primer Nivel/reto1.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+print("Escribe tu nombre: ")
+valor = input()
+print("hola "+ valor)
---
+++ Python/Primer Nivel/reto2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+print("Escribe tu nombre: ")
+valor = input()
+print("Escribe tu apellido: ")
+valor2 = input()
+print("hola "+ valor + " " + valor2)
