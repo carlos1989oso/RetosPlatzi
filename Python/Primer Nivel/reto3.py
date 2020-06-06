202bf69703d5979a00c8dc334e4ec7e0fa8012fc
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 02:26:54 -0500

    Reto 3

--- Python/Primer Nivel/reto3.py
+++ Python/Primer Nivel/reto3.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+CATEGORIES = ['Desarrollo e Ingenieria','Diseño y UX', 'Marketing','Negocios y emprendimiento','Producción Audiovisual','Crecimiento Profesional']
+
+print ("Platzi cuenta con cursos de:\n")
+for i in range (len(CATEGORIES)):
+  print("-- " + CATEGORIES[i] + "\n")
