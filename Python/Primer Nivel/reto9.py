41cf05d664c5975422802905927cce168b44f80f
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 03:32:37 -0500

    reto 9

--- Python/Primer Nivel/reto9.py
+++ Python/Primer Nivel/reto9.py
@@ -1,1 +1,29 @@
 # -*- coding: utf-8 -*-
+
+import random
+import math 
+
+def daysToHours(days):
+  return days * 24
+
+def hoursToMinutes(hours):
+  return hours * 60
+
+def minutesToSeconds(minutes):
+  return minutes * 60
+
+diasCount = float(input('\n¿Cuantos dias quieres calcular?: '))
+
+hoursCoverted = daysToHours(diasCount)
+hoursInt = math.trunc(hoursCoverted)
+hoursRes = hoursCoverted - hoursInt
+
+minutesConverted = hoursToMinutes(hoursRes)
+minutesInt = math.trunc(minutesConverted)
+minutesRes = minutesConverted - minutesInt
+
+secondsConverted = minutesToSeconds(minutesRes)
+secondsConverted = math.trunc(secondsConverted)
+
+print("\n\nEn " + str(diasCount)  + " dias hay " + str(hoursInt) + " horas, " + str(minutesInt) + " minutos y " + str(secondsConverted) + " segundos")
+
