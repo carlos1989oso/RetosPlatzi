00ad63d9e2c02c899b8a97d2fc7fd5860bbaeddd
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 02:59:21 -0500

    reto 7

--- Python/Primer Nivel/reto7.py
+++ Python/Primer Nivel/reto7.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+
+print("Escribe tu nombre: ")
+nameUser = input()
+edadUser = int(input('\n¿Cuantos años tienes: '))
+print("\n" + nameUser + " el año pasado tenías " + str(edadUser - 1) + " años y el próximo año cumplirás " + str(edadUser + 1) + " años.")
