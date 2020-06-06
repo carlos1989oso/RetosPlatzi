3a7f759f583e3872e47de23fb35dd2f19468c75d
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 13:18:39 -0500

    reto 5

---
+++ Python/Tercer Nivel/reto5.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import math
+
+word1 = input("\nEscribe la primera palabra: ")
+word2 = input("\nEscribe la segunda palabra: ")
+print("\n---------------------------------\n")
+print("\n" + word1.upper())
+print("\n" + word2.lower())
