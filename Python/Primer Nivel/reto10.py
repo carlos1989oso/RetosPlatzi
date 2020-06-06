d8664f29dad698c26ebcfe5926b51e911955f08e
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-06 03:36:07 -0500

    reto11

--- Python/Primer Nivel/reto10.py
+++ Python/Primer Nivel/reto10.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+import math 
+
+millasUser = float(input('\n¿Cuantas millas quieres convertir a kilometros?: '))
+
+kmUser = 1.609344 * millasUser
+
+print("\n\nEn " + str(round(millasUser,2))  + " millas hay " + str(round(kmUser,2)) + " kilometros")
+
